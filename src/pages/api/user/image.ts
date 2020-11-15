5f8ec3a65706e3917d3022f69db7d9738be808c3
Okubo <aspastrana990@gmail.com>
2020-11-15 01:31:59 -0300

    first commit

--- src/pages/api/user/image.ts
+++ src/pages/api/user/image.ts
@@ -39,14 +39,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       const token = nanoid();
       const timeStamp = dayjs().format("DD-MM-YYYY");
 
-      // fs.mkdir(`./images/${timeStamp}`, { recursive: true }, (err) => {
-      //   if (err) {
-      //     return res.status(400).json({
-      //       msg: "no hay directorio",
-      //       err: err,
-      //     });
-      //   }
-      // });
+      fs.mkdirSync(`./images/${timeStamp}`, { recursive: true });
 
       const FBuser = await db.doc(`/users/${user.user}`).get();
       const image = FBuser.data().imageUrl;
