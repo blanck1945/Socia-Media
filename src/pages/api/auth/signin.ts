e83dd6d65bc0bbca7482ff74a0cc975c673d93bb
Okubo <aspastrana990@gmail.com>
2020-12-01 23:29:34 -0300

    social

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -6,70 +6,73 @@ import jwt from "jsonwebtoken";
 import dbConnect from "../../../../mongo/mongo.db";
 import { validateYupSchema } from "../../../../YupSchemas/YupSchemas";
 import { SignInSchema } from "../../../validators/AuthSchema";
+import { CorsMiddleware } from "../../../utils/CorsMiddleware";
 
-export default validateYupSchema(
-  SignInSchema,
-  async (req: NextApiRequest, res: NextApiResponse) => {
-    await dbConnect();
+export default CorsMiddleware(
+  validateYupSchema(
+    SignInSchema,
+    async (req: NextApiRequest, res: NextApiResponse) => {
+      await dbConnect();
 
-    const { method } = req;
+      const { method } = req;
 
-    switch (method) {
-      case "POST":
-        try {
-          const { email } = req.body;
+      switch (method) {
+        case "POST":
+          try {
+            const { email } = req.body;
 
-          const mongoUser = await Usuario.findOne({ email });
+            const mongoUser = await Usuario.findOne({ email });
 
-          if (!mongoUser) {
+            if (!mongoUser) {
+              return res.status(404).json({
+                msg: "No user was authenticated",
+              });
+            }
+
+            const mongoLikes = await Like.find({
+              name: mongoUser.name,
+            });
+
+            let mongoImgString = "";
+
+            if (mongoUser.avatar) {
+              const mongob64 = Buffer.from(mongoUser.avatar, "base64");
+              mongoImgString = `data:${mongoUser.type};base64,${mongob64}`;
+              mongoUser.set("avatar", null);
+            }
+
+            const token = jwt.sign(
+              { id: mongoUser._id, name: mongoUser.name },
+              process.env.JWT_SECRET
+            );
+            res.setHeader(
+              "Set-Cookie",
+              cookie.serialize("auth", token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV !== "development",
+                sameSite: "strict",
+                maxAge: 3600 * 2,
+                path: "/",
+              })
+            );
+
+            res.status(200).json({
+              msg: "Usuario loggeado",
+              userData: {
+                credentials: {
+                  ...mongoUser.toJSON(),
+                  mongoImgString,
+                },
+                likes: mongoLikes,
+              },
+            });
+          } catch (err) {
             return res.status(404).json({
-              msg: "No user was authenticated",
+              msg: "Error",
+              err: err,
             });
           }
-
-          const mongoLikes = await Like.find({
-            name: mongoUser.name,
-          });
-
-          let mongoImgString = "";
-
-          if (mongoUser.avatar) {
-            const mongob64 = Buffer.from(mongoUser.avatar, "base64");
-            mongoImgString = `data:${mongoUser.type};base64,${mongob64}`;
-            mongoUser.set("avatar", null);
-          }
-
-          const token = jwt.sign(
-            { id: mongoUser._id, name: mongoUser.name },
-            process.env.JWT_SECRET
-          );
-          res.setHeader(
-            "Set-Cookie",
-            cookie.serialize("auth", token, {
-              httpOnly: true,
-              secure: process.env.NODE_ENV !== "development",
-              sameSite: "strict",
-              maxAge: 3600 * 2,
-              path: "/",
-            })
-          );
-
-          res.status(200).json({
-            msg: "Usuario loggeado",
-            userData: {
-              credentials: {
-                ...mongoUser.toJSON(),
-                mongoImgString,
-              },
-              likes: mongoLikes,
-            },
-          });
-        } catch (err) {
-          return res.status(404).json({
-            msg: "Error",
-            err: err,
-          });
-        }
+      }
     }
-  }
+  )
 );
---
+++ src/utils/CorsMiddleware.ts
@@ -0,0 +1,21 @@
+import Cors from "cors";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import { validateYupSchema } from "../../YupSchemas/YupSchemas";
+import { SignInSchema } from "../validators/AuthSchema";
+import authMiddleware from "./InitMiddleware";
+
+// Initialize the cors middleware
+const cors = authMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+
+export const CorsMiddleware = (fn: NextApiHandler) => async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  await cors(req, res);
+};
